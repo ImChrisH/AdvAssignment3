96a9b1871d12f8984ca8d1e2592e31d964bacbd7
ImChrisH <hui.simplicity@gmail.com>
2024-08-27 21:15:40 -0400

    adding files test

---
+++ src/employees.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const employees = [
+  {
+    empID: 1,
+    name: "Jeff Bezos",
+    position: "CEO",
+    hobby: "Sitting on piles of cash",
+    imgURL:
+      "https://assets.entrepreneur.com/content/3x2/2000/20150224165308-jeff-bezos-amazon.jpeg",
+  },
+  {
+    empID: 2,
+    name: "Harry Potter",
+    position: "Wizard",
+    hobby: "Getting into trouble",
+    imgURL:
+      "https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/09/12/17/potter.jpg",
+  },
+  {
+    empID: 3,
+    name: "Deadpool",
+    position: "Mercenary",
+    hobby: "Collecting weapons",
+    imgURL:
+      "https://miro.medium.com/v2/resize:fit:3840/format:webp/1*0ubYRV_WNR9iYrzUAVINHw.jpeg",
+  },
+];
+
+export default employees;
